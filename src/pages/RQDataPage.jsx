12d23f638bf160c95b1b7a5a67890f0772741e29
Muhammad Abdullah Zahid <119707334+abdullah1904@users.noreply.github.com>
2024-05-27 01:21:52 +0500

    Lesson-11 (Data Transformation)

---
+++ src/pages/RQDataPage.jsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useQuery } from "react-query"
+
+const fetchUsers = () => {
+    return axios.get('https://jsonplaceholder.typicode.com/users');
+}
+
+
+const RQDataPage = () => {
+    const onSuccess = (data)=>{
+        console.log('Perform side effect after data fetching',data);
+    }
+    const onError = (error)=>{
+        console.log('Perform side effect after countering error',error);
+    }
+    const { isLoading, data, isError, error, isFetching , refetch} = useQuery('Users', fetchUsers,{
+        onSuccess,
+        onError,
+        select:(data)=>{
+            const users = data.data.map(user=> user.name);
+            return users;
+        }
+    });
+    console.log(isLoading, isFetching);
+    if (isLoading || isFetching) {
+        return <h2>Loading...</h2>
+    }
+    if (isError) {
+        return <h2>{error.message}</h2>
+    }
+    return (
+        <>
+            <h2>RQ User Data Page</h2>
+            <button onClick={refetch}>Fetch Users</button>
+            <ul >
+                {/* {data?.data.map(user => {
+                    return <li key={user.id}>{user.name}</li>
+                })} */}
+                {data.map(user => {
+                    return <li key={user}>{user}</li>
+                })}
+            </ul>
+        </>
+    )
+}
+
+export default RQDataPage
