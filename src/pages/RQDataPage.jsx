35d4159c5dd55f3d5c096df072eff237e84f26f0
Muhammad Abdullah Zahid <119707334+abdullah1904@users.noreply.github.com>
2024-05-26 19:26:11 +0500

    Lesson-4 (React Query Devtools)

---
+++ src/pages/RQDataPage.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useQuery } from "react-query"
+
+const fetchUsers = ()=>{
+    return axios.get('https://jsonplaceholder.typicode.com/users');
+}
+
+
+const RQDataPage = () => {
+    const {isLoading, data, isError, error} = useQuery('Users',fetchUsers);
+    if (isLoading) {
+        return <h2>Loading...</h2>
+    }
+    if(isError){
+        return <h2>{error.message}</h2>
+    }
+    return (
+        <>
+            <h2>RQ User Data Page</h2>
+            <ul >
+                {data?.data.map(user => {
+                    return <li key={user.id}>{user.name}</li>
+                })}
+            </ul>
+        </>
+    )
+}
+
+export default RQDataPage
