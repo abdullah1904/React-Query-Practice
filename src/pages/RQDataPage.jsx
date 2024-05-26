e99011a8e497d71abae01198c259225b79b977d5
Muhammad Abdullah Zahid <119707334+abdullah1904@users.noreply.github.com>
2024-05-27 00:51:37 +0500

    Lesson-8 (Polling)

---
+++ src/pages/RQDataPage.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useQuery } from "react-query"
+
+const fetchUsers = () => {
+    return axios.get('https://jsonplaceholder.typicode.com/users');
+}
+
+
+const RQDataPage = () => {
+    const { isLoading, data, isError, error, isFetching } = useQuery('Users', fetchUsers, { 
+        refetchInterval: 5 * 1000,
+        refetchIntervalInBackground: true
+    });
+    console.log(isLoading, isFetching);
+    if (isLoading) {
+        return <h2>Loading...</h2>
+    }
+    if (isError) {
+        return <h2>{error.message}</h2>
+    }
+    return (
+        <>
+            <h2>RQ User Data Page</h2>
+            <ul >
+                {data?.data.map(user => {
+                    return <li key={user.id}>{user.name}</li>
+                })}
+            </ul>
+        </>
+    )
+}
+
+export default RQDataPage
