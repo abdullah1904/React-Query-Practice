1e21fa89aa1fbe6310164914cba028da472bb134
Muhammad Abdullah Zahid <119707334+abdullah1904@users.noreply.github.com>
2024-05-27 01:11:40 +0500

    Lesson-10 (Success and Error Callbacks)

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import './App.css'
+import Home from './pages/Home'
+import DataPage from './pages/DataPage'
+import RQDataPage from './pages/RQDataPage'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/data-page'>Traditional Data Page</Link></li>
+              <li><Link to='/rq-data-page'>RQ Super Heroes</Link></li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='/data-page' element={<DataPage />} />
+            <Route path='/rq-data-page' element={<RQDataPage />} />
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
+    </>
+  )
+}
+
+export default App
