141c0666a98b22791b2c5bc2af0daf740a594828
Muhammad Abdullah Zahid <119707334+abdullah1904@users.noreply.github.com>
2024-05-26 19:02:26 +0500

    Lesson-2 (React Query Fetching)

---
+++ src/pages/DataPage.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const DataPage = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users').then(res => {
+            setData(res.data);
+            setIsLoading(false);
+        })
+    }, []);
+    if (isLoading) {
+        return <h2>Loading...</h2>
+    }
+    return (
+        <>
+            <h2>User Data Page</h2>
+            <ul >
+                {data.map(user => {
+                    return <li key={user.id}>{user.name}</li>
+                })}
+            </ul>
+        </>
+    )
+
+}
+
+export default DataPage
